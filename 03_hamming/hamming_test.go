f43492a63c2ee4f28e2854edea7b1dbfebc0de48
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-04-08 21:48:19 -0700

    Convert int stream merge test to table-driven

--- 03_hamming/hamming_test.go
+++ 03_hamming/hamming_test.go
@@ -197,6 +197,7 @@ func TestIntegerStreamMerge_Marshall(t *testing.T) {
 		bValues  []int
 		expected []int
 	}{
+		{[]int{}, []int{}, []int{}},
 		{[]int{1, 3, 5, 7, 9}, []int{2, 4, 6, 8, 10}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
 	}
 
