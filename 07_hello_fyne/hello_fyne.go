f8c73a90ea26f78bd2ca629142a732b8073f7453
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-05-05 11:49:39 -0700

    finished hello world demo for fyne

--- 07_hello_fyne/hello_fyne.go
+++ 07_hello_fyne/hello_fyne.go
@@ -1,12 +1,22 @@
 package main
 
-import "fyne.io/fyne/app"
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
 
 func main() {
 
 	app := app.New()
 
 	win := app.NewWindow("Hello World")
+
+	win.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello World!"),
+		widget.NewButton("Quit", func() {
+			app.Quit()
+		}),
+	))
 
 	win.ShowAndRun()
 }
